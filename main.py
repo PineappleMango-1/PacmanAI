a457759042e18a33bd0113ceeea370cced2ddac5
Gido-Alberts <62379663+Gido-Alberts@users.noreply.github.com>
2020-03-20 16:31:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 print("Hello World")
 print("bananacruise")
-<<<<<<< HEAD
 ##One more test
 print("This is Gido testing")
 print("Test#2")
-=======
 #One more test
 print("Hallo Alex")
 
->>>>>>> 829242aca8de948633fa2e6f0c118554b001ba67
