0291f5468db7dd56e4194032dfcb97872985fe90
PineappleMango-1 <y.h.braakman@student.utwente.nl>
2020-04-02 11:38:09 +0200

    Can be removed

--- Pacman_Gym_Environment.py
+++ Pacman_Gym_Environment.py
@@ -1,4 +1,6 @@
 import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
 from pacman_game import PacmanGame
 
 class PacmanEnv(gym.Env):
@@ -6,17 +8,17 @@ class PacmanEnv(gym.Env):
     def __init__(self):
         print("Init")
 
-    def step(self):
+    def step(self, action):
         print("Foo")
 
     def reset(self):
         print("Foo")
 
-    def render(self):
+    def render(self, mode='human'):
         print("Foo")
 
 
 
 env = gym.make('CartPole-v0')
 env.reset()
-env.render()
+
